329dd872f19596de0c87c5dfc7a019e5c4e0e8f4
sregea <aitorpegea@gmail.com>
2018-09-17 19:31:26 +0200

    First draft of snake

--- snake/sketch.js
+++ snake/sketch.js
@@ -1,136 +1,63 @@
-var ball;
-var wall;
+let snake;
+let dir = 1;
 
 function setup() {
-	createCanvas(500, 400);
+	createCanvas(800, 600);
 
-	// create a ball and a rectangle
-	ball = new Ball();
-	wall = new Rectangle();
+	// create an array of possible x and y positions for the snake
+	snake = new Snake();
+}
+
+
+
+function keyPressed() {
+	if (keyCode === RIGHT_ARROW) {
+		dir = 1;
+	}
+	else if (keyCode === LEFT_ARROW) {
+		dir = -1;
+	}
+	/*
+	else if (keyCode === UP_ARROW) {
+		dir = 2;
+	}
+	else if (keyCode === DOWN_ARROW) {
+		dir = 3;
+	}
+	*/
 }
 
 function draw() {
 	background(150);
 
-	// draw the rectangle, at the same location always
-	fill(135, 206, 250);
-	wall.draw();
+	console.log(dir);
 
-	// move the ball and then check for collisions
-	noStroke();
-	fill(0, 0, 255);
-	ball.bounceEdge();
-	ball.move();
-	ball.draw();
-	ball.bounceRect(wall);
+	snake.update();
+	fill(0);
+	snake.draw();
 }
 
-class Ball {
+class Snake {
 	constructor() {
-		this.rad = 15;
-		this.x = random(0, width);
-		this.y = random(0, height);
-		this.xspeed = random(3, 7);
-		this.yspeed = random(3, 7);
+		this.body = [];
+		this.body[0] = new SnakeElement(40, 40);
+
+	}
+
+	update() {
+		// update following a cartesian coordinate system
+		// this.body[0].x = this.body[0].x + dir[0];
+		// this.body[0].y = this.body[0].y + dir[1];
 	}
 
 	draw() {
-		ellipse(this.x, this.y, 2*this.rad, 2*this.rad);
-	}
-
-	move() {
-		this.x = this.x + this.xspeed;
-		this.y = this.y + this.yspeed;
-	}
-
-	bounceEdge() {
-		// check in the right and left edges
-		if (this.x > width || this.x < 0) {
-			this.changeXspeed();
-		}
-
-		// check in the upper and lower edges
-		if (this.y > height || this.y < 0) {
-			this.changeYspeed();
-		}
-	}
-
-	changeXspeed() {
-		this.xspeed = -this.xspeed;
-	}
-
-	changeYspeed() {
-		this.yspeed = -this.yspeed;
-	}
-
-	bounceRect(r) {
-		// first check the sides of the rectangle
-		if (this.x < r.right + this.rad && this.x > r.right && this.y > r.upper && this.y < r.lower) {
-			this.changeXspeed();
-		}
-
-		if (this.x < r.left && this.x > r.left - this.rad && this.y > r.upper && this.y < r.lower) {
-			this.changeXspeed();
-		}
-
-		if (this.y > r.upper - this.rad && this.y < r.upper && this.x > r.left && this.x < r.right) {
-			this.changeYspeed();
-		}
-
-		if (this.y > r.lower && this.y < r.lower + this.rad && this.x > r.left && this.x < r.right) {
-			this.changeYspeed();
-		}
-
-		// now check the edges of the rectangle
-		if (this.x < r.left && this.y < this.upper && dist(r.left, r.upper, this.x, this.y) < this.rad) {
-			this.bounceCorner(r);
-		}
-
-		if (this.x > r.right && this.y < r.upper && dist(r.right, r.upper, this.x, this.y) < this.rad) {
-			this.bounceCorner(r);
-		}
-
-		if (this.x < r.left && this.y > r.lower && dist(r.left, r.lower, this.x, this.y) < this.rad) {
-			this.bounceCorner(r);
-		}
-
-		if (this.x > r.right && this.y > r. lower && dist(r.right, r.lower, this.x, this.y) < this.rad) {
-			this.bounceCorner(r);
-		}
-	}
-
-	bounceCorner(r) {
-
-	}
-
-	cornerAngle(x, y, rx, ry) {
-		return Math.atan((y - ry)/(x - rx)) % 90;
+		rect(this.body[0].x, height - this.body[0].y, 20, 20);
 	}
 }
 
-// class for the rectangle
-class Rectangle {
-	constructor() {
-		this.x = random(250, 350);
-		this.y = random(100, 250);
-		this.rectHeight = 150;
-		this.rectWidth = 50;
-
-		this.left = this.x;
-		this.right = this.x + this.rectWidth;
-		this.upper = this.y;
-		this.lower = this.y + this.rectHeight;
-	}
-
-	draw() {
-		rect(this.x, this.y, this.rectWidth, this.rectHeight);
-	}
-
-	frontSlash(x) {
-		return this.upper + (this.lower - this.upper)/(this.left - this.right)*(x - this.right);
-	}
-
-	backSlash(x) {
-		return this.upper + (this.lower - this.upper)/(this.right - this.left)*(x - this.left);
+class SnakeElement {
+	constructor(x, y) {
+		this.x = x;
+		this.y = y;
 	}
 }

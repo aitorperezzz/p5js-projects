e9cc6e5843709f80e467ebcbe9d676c1f02115c2
sregea <aitorpegea@gmail.com>
2018-09-26 13:18:55 +0200

    The camera moves on a plane, and the edges of the cube are shown

--- cube_view/sketch.js
+++ cube_view/sketch.js
@@ -1,16 +1,50 @@
 let cube;
-let myCamera;
+let myCam;
+
+let step = 5;
 
 function setup() {
 	createCanvas(800, 600);
 
-	cube = new Cube(0, 0, 0, 100);
-	myCamera = new Camera(50, 0, 0, [-1, 0, 0], 50);
+	cube = new Cube(0, 0, 0, 30);
+	myCam = new Camera(100, 0, 0, [-1, 0, 0], 30);
 }
 
 function draw() {
-	// cube,draw(camera);
+	background(150);
 
+	cube.draw(myCam);
+}
+
+function keyPressed() {
+	if (keyCode === RIGHT_ARROW) {
+		myCam.moveRight();
+	}
+	if (keyCode === LEFT_ARROW) {
+		myCam.moveLeft();
+	}
+	if (keyCode === UP_ARROW) {
+		myCam.moveUp();
+	}
+	if (keyCode === DOWN_ARROW) {
+		myCam.moveDown();
+	}
+}
+
+/*
+function mousePressed() {
+	myCam.updateDirection(mouseX, mouseY);
+}
+*/
+
+class Point2D {
+	// A 2D point is a point that belongs in a plane (normally the
+	// plane of view of a camera)
+	constructor(x, y) {
+		this.x = x;
+		this.y = y;
+		this.position = [this.x, this.y];
+	}
 }
 
 class Point {
@@ -20,6 +54,7 @@ class Point {
 		this.x = x;
 		this.y = y;
 		this.z = z;
+		this.position = [this.x, this.y, this.z];
 	}
 }
 
@@ -61,19 +96,67 @@ class Cube {
 			}
 			this.points.push(newPoint);
 		}
+
+		// Lastly, create the floor and roof of the cube, for easily drawing the
+		// edges later
+		this.floor = [this.points[0], this.points[1], this.points[3], this.points[2]];
+		this.roof = [this.points[4], this.points[5], this.points[7], this.points[6]];
+
 	}
 
 	draw(cam) {
-		// draw the cube as viewed from the camera
-		let projPoints = [];
-		for (let i = 0; i < this.points.length; i++) {
-			projPoints.push(cam.giveProjection(this.points[i]));
+		// First call displayPoints to get the projections of each point, as 2D
+		// point objects that belong in the current plane of view of the camera
+		let projPoints = this.displayPoints(cam);
+
+		// Paint each point in the canvas using canvasPosition
+		let canvasPos = [];
+		for (let i = 0; i < 2; i++) {
+			let newLine = [];
+			for (let j = 0; j < 4; j++) {
+				let x = cam.canvasPosition(projPoints[i][j])[0];
+				let y = cam.canvasPosition(projPoints[i][j])[1];
+				newLine.push([x, y]);
+			}
+			canvasPos.push(newLine);
 		}
 
-		// Draw the lines between the relevant points
-		// line();
-		// line();
+		// Now we have the canvas positions, we have to draw
+		for (let j = 0; j < 4; j++) {
+			// Draw the points as ellipses
+			for (let i = 0; i < 2; i++) {
+				// No need to draw the points now
+				/*
+				noStroke();
+				fill(0);
+				ellipse(canvasPos[i][j][0], canvasPos[i][j][1], 5, 5); */
 
+				// Draw the lines on the floor and on the roof
+				stroke(0);
+				strokeWeight(4);
+				line(canvasPos[i][j % 4][0], canvasPos[i][j % 4][1], canvasPos[i][(j + 1) % 4][0], canvasPos[i][(j + 1) % 4][1]);
+			}
+			// Draw the vertical lines of the cube
+			stroke(0);
+			strokeWeight(4);
+			line(canvasPos[0][j][0], canvasPos[0][j][1], canvasPos[1][j][0], canvasPos[1][j][1]);
+		}
+	}
+
+	displayPoints(cam) {
+		// Get the projection of the floor
+		let projFloor = [];
+		for (let i = 0; i < this.floor.length; i++) {
+			let newProj = cam.giveProjection(this.floor[i]);
+			projFloor.push(newProj);
+		}
+		// Get the projection of the roof
+		let projRoof = [];
+		for (let i = 0; i < this.roof.length; i++) {
+			let newProj = cam.giveProjection(this.roof[i]);
+			projRoof.push(newProj);
+		}
+		return [projFloor, projRoof];
 	}
 }
 
@@ -84,51 +167,128 @@ class Camera {
 		this.x = x;
 		this.y = y;
 		this.z = z;
-		this.location = [this.x, this.y, this.z];
+		this.position = [this.x, this.y, this.z];
 		this.xaxis = [0, 1, 0];
 		this.dir = direction;
 		this.zaxis = [1, 0, 0];
-		console.log(this.xaxis);
-		console.log(this.zaxis);
 		this.yaxis = vectorProduct(this.zaxis, this.xaxis);
-		console.log(this.yaxis);
 		this.d = distance;
 		this.xlimit = this.d;
 		this.ylimit = height * this.d / width;
 	}
 
+	updatePosition() {
+		this.position = [this.x, this.y, this.z];
+	}
+
+	updateDirection(mx, my) {
+		let point = this.canvasPositionInverse(mx, my);
+
+		let inPlane = [point.x, point.y, -this.d];
+		let Mmatrix = matrixBasis(this.xaxis, this.yaxis, this.zaxis);
+		let result = matrixProduct(Mmatrix, vectorCol(inPlane));
+		this.dir = vectorNormalize(result);
+		this.updateAxes();
+	}
+
+	updateAxes() {
+		this.zaxis = vectorScalar(-1, this.dir);
+		this.giveXaxis();
+		this.yaxis = vectorProduct(this.zaxis, this.xaxis);
+	}
+
+	giveXaxis() {
+		// Given the current direction, give an x axis which is parallel to the ground
+		let vector = [];
+		vector[0] = this.dir[1];
+		vector[1] = -this.dir[0];
+		vector[2] = 0;
+		return vectorNormalize(vector);
+	}
+
+	/*
 	computeYAxis() {
 		// gives the axes of the camera in the cartesian coordinate system
 		this.yaxis = matrixProduct(this.z, this.x);
-	}
+	} */
 
 	giveProjection(point) {
-		// receives a point and gives the coordinates in the camera's
-		// coordinate system
-		let vector = vectorSum(point.location, vectorNeg(this.location));
-		let Mmatrix = matrixBasis(camera.xaxis, camera.yaxis, camera.zaxis);
-		let result = matrixProduct(matrixInverse(Mmatrix), vectorCol(vector));
+		// Receives a 3D point and gives the corresponding 2D point,
+		// as viewed from the camera
+		let vector = vectorSum(point.position, vectorNeg(this.position));
+		let Mmatrix = matrixBasis(this.xaxis, this.yaxis, this.zaxis);
+		let result = vectorRow(matrixProduct(matrixInverse(Mmatrix), vectorCol(vector)));
 
 		// Now apply projection rules to that vector (result)
-		// Multiply the vector by
-		let vectorinplane = vectorScalar(- this.d / this.z, result);
-		vectorinplane.splice(2, 1);
-		let pos = canvasPosition(vectorinplane);
+		let inPlane = vectorScalar(-this.d / result[2], result);
+		inPlane.splice(2, 1);
+		let projPoint = new Point2D(inPlane[0], inPlane[1]);
+		return projPoint;
 	}
 
-	canvasPosition(vector) {
-		if (this.inFov(vector)) {
-			let xCanvas = map(vector[0], -this.xlimit, this.xlimit, 0, width);
-			let yCanvas = map(vector[1], -this.ylimit, this.ylimit, 0, height);
-			return map()
-		}
-
+	/*
+	reverseProjection(point) {
+		// Receives a 2D point in the plane of vision and returns the 3D point
+		// that belongs inside the plane of vision
+		let inPlane = point.position.push(-this.d);
+		let vector = vectorSum(this.position, inPlane);
+		let Mmatrix = matrixBasis(this.xaxis, this.yaxis, this.zaxis);
+		let result = matrixProduct(Mmatrix, vectorCol(vector));
+		this.dir = result;
 	}
 
-	fieldOfView() {
+	/*
+	canvasLimits() {
+		// Updates the canvas limits
+		this.xlimit = this.d;
+		this.ylimit = height * this.d / width;
+	} */
+
+	canvasPosition(point) {
+		// Receives a 2D point and gives the coordinates for use in p5js
+		let x = map(point.x, -this.xlimit, this.xlimit, 0, width);
+		let y = map(point.y, -this.ylimit, this.ylimit, height, 0);
+		return [x, y];
+	}
+
+	canvasPositionInverse(mx, my) {
+		// Receives an x and a y in the canvas and returns x and y coordinates
+		// in the plane of view (returns a type Point2D)
+		let x = map(mx, 0, width, -this.xlimit, this.xlimit);
+		let y = map(my, 0, height, this.ylimit, -this.ylimit);
+		let newPoint = new Point2D(x, y);
+		return newPoint;
+	}
+
+	/*
+	fieldOfView(point2D) {
+		// Receives a 2D point in the plane of view and decides if it is inside
+
+
+
 		// Gives the x and y limits in the field of view of the camera,
 		// this takes into account the distance of the camera to the plane
 		this.xlimit = 2 * this.d;
 		this.ylimit = heigth * 2 * this.d / width;
+	} */
+
+	moveRight() {
+		this.y = this.y + step;
+		this.updatePosition();
+	}
+
+	moveLeft() {
+		this.y = this.y - step;
+		this.updatePosition();
+	}
+
+	moveUp() {
+		this.z = this.z + step;
+		this.updatePosition();
+	}
+
+	moveDown() {
+		this.z = this.z - step;
+		this.updatePosition();
 	}
 }
--- cube_view/vectorfunctions.js
+++ cube_view/vectorfunctions.js
@@ -42,6 +42,15 @@ function vectorCol(vector) {
 	return result;
 }
 
+function vectorRow(vector) {
+	// Receives a column vector and returns a row vector
+	let result = [];
+	result.push(vector[0][0]);
+	result.push(vector[1][0]);
+	result.push(vector[2][0]);
+	return result;
+}
+
 function vectorScalar(num, vector) {
 	// Multiply the number by the vector
 	let result = [];
@@ -49,4 +58,19 @@ function vectorScalar(num, vector) {
 		result.push(num * vector[i]);
 	}
 	return result;
+}
+
+function vectorNorm(vector) {
+	// Receives a vector and outputs the norm
+	let norm = 0;
+	for (let i = 0; i < vector.length; i++) {
+		norm += Math.pow(vector[i], 2);
+	}
+	return Math.sqrt(norm);
+}
+
+function vectorNormalize(vector) {
+	// Receives a vector and normalizes it
+	let norm = vectorNorm(vector);
+	return vectorScalar(1/norm, vector);
 }
